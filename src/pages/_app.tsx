0031dac040a2d31c027a631fd3081a38976a8863
Allan Camargo <allan.cdev@gmail.com>
2023-05-06 09:51:00 -0300

    init repo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
+import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from '@/styles/pages/app';
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {  
+  return (
+    <Container>
+      <Header>
+          <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,11 @@
+import { styled } from "..";
+
+export const HomeContainer = styled('main', {
+    display: 'flex',
+    gap: '3rem',
+    background: 'red',
+})
+
+export const Product = styled('div', {
+
+})
