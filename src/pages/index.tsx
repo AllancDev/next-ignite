5ccc74b348c3552f561899f80afbeddf7311ddb0
Allan Camargo <allan.cdev@gmail.com>
2023-05-07 19:29:16 -0300

    feat: add home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
 import { HomeContainer, Product } from "@/styles/pages/home";
 
+import { useKeenSlider } from 'keen-slider/react';
+
 import Image from "next/image";
 
 import tShirt1 from '../assets/t-shirts/1.png';
 import tShirt2 from '../assets/t-shirts/2.png';
 import tShirt3 from '../assets/t-shirts/3.png';
 
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
+    const [ sliderRef ] = useKeenSlider({
+        slides: {
+            perView: 2,
+            spacing: 45
+        }
+    })
+
     return (
-        <HomeContainer>
-            <Product>
+        <HomeContainer ref={sliderRef} className="keen-slider">
+            <Product className="keen-slider__slide">
                 <Image src={tShirt1} width={520} height={480} alt="" />
 
                 <footer>
@@ -18,8 +29,18 @@ export default function Home() {
                 </footer>
 
             </Product>
-            <Product>
+            <Product className="keen-slider__slide">
                 <Image src={tShirt2} width={520} height={480} alt="" />
+
+                <footer>
+                    <strong>Camiseta X</strong>
+                    <span>R$ 79,90</span>
+                </footer>
+
+            </Product>
+
+            <Product className="keen-slider__slide">
+                <Image src={tShirt3} width={520} height={480} alt="" />
 
                 <footer>
                     <strong>Camiseta X</strong>
