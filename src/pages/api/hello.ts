f0da52832351e11ebe81fd6b4638d2d26dbb3e79
Allan Camargo <allan.cdev@gmail.com>
2023-05-12 17:10:36 -0300

    feat: add api routes

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,18 @@
+import { stripe } from "@/lib/stripe";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    return res.json({ message: 'Hello World' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const priceId = '';
+    
+    const checkoutSession = await stripe.checkout.sessions.create({
+        success_url: '',
+        
+        mode: 'payment',
+        line_items: [
+            {
+                price: priceId,
+                quantity: 1,
+            }
+        ]
+    })
 }
